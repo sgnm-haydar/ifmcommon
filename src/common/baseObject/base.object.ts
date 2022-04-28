092a6adfc78b71fb5ec808e4d570c3fe443711ec
sgnm-haydar <haydar.urdogan@signumtte.com>
2022-04-28 10:30:53 +0300

    update base mongo object

--- src/common/baseObject/base.object.ts
+++ src/common/baseObject/base.object.ts
@@ -1,5 +1,7 @@
+import { Prop } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-
+import { v4 as uuidv4 } from 'uuid';
+import { genCurrentDate } from '../func/generate.new.date';
 /**
  * Base object for entities whic inherit mongoose document meytods
  */
@@ -7,17 +9,38 @@ export abstract class BasePersistantDocumentObject extends Document {
   /**
    * Uuid for uniqeness
    */
+  @Prop({
+    type: String,
+    default: function genUUID() {
+      return uuidv4();
+    },
+  })
   uuid: string;
-  /**
-   * location
-   */
-  locations: string;
+  @Prop({
+    type: Date,
+    default: genCurrentDate(),
+  })
+  createdAt: Date;
+
+  @Prop({
+    type: Date,
+    default: genCurrentDate(),
+  })
+  updatedAt: Date;
   /**
    * isActive
    */
+  @Prop({
+    type: Boolean,
+    default: true,
+  })
   isActive: boolean;
   /**
    * isDeleted
    */
+  @Prop({
+    type: Boolean,
+    default: false,
+  })
   isDeleted: boolean;
 }
