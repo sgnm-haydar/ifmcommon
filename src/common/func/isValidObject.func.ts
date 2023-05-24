cfb7f35d89066bbac47d48e9cfc447bb53efb11b
ombayus <omerbayramcavus@gmail.com>
2023-05-24 10:55:25 +0300

    add isvalidobject to index.ts

--- src/common/func/isValidObject.func.ts
+++ src/common/func/isValidObject.func.ts
@@ -1,16 +1,14 @@
 import { BadRequestException } from "@nestjs/common";
 import { validate } from 'class-validator';
 
-const isValidObject = async (obj: object,labelOfBody:string,type:any)=>{
-    const temp = new type();
-    Object.assign(temp,JSON.parse(obj[labelOfBody]));
-    const errors = await validate(temp);
+export async function isValidObject(obj: object,labelOfBody:string,type:any){
+  const temp = new type();
+  Object.assign(temp,JSON.parse(obj[labelOfBody]));
+  const errors = await validate(temp);
 
-    if(errors.length > 0){
-      throw new BadRequestException(errors.toString());
-    }
-    return errors.length == 0;
+  if(errors.length > 0){
+    throw new BadRequestException(errors.toString());
   }
-
-export default isValidObject;
+  return errors.length == 0;
+}
 
